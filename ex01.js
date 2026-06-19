9ac4cce293c7991795f229ae868e1ed6e95facbf
Victor matheus tempesta casemiro <victorcasemiro19@gmail.com>
2026-06-19 10:53:49 -0300

    ex01 feito

--- ex01.js
+++ ex01.js
@@ -1,12 +1,19 @@
-function sortearVariosNumeros(quantidade, min, max) {
-    const numerosSorteados = [];
-    while (numerosSorteados.length < quantidade) {
-        const numero = Math.floor(Math.random() * (max - min + 1)) + min;
-        if (!numerosSorteados.includes(numero)) {
-            numerosSorteados.push(numero);
+//sortear 20 numeros entre 0 e 10
+count = 0
+divs = 0
+    for (i = 0; i < 21; i++){
+        sorteado = Math.floor(Math.random () *10);
+        console.log(`Sorteio número ${i}`);
+        console.log(`Sorteado ${sorteado}`)
+        if (sorteado > 5) {
+            count = count + 1;
+        }
+
+        if (sorteado % 3 == 0){
+            divs = divs + 1;
         }
     }
-    return numerosSorteados.sort((a, b) => a - b); // Ordena os números
-}
 
-console.log(sortearVariosNumeros(20, 1, 100));
+    console.log(`Neste sorteio foram sorteados ${count} núemros maiores que 5, e ${divs} núemros divisiveis por 3`)
+    //mostras quantos são maiores que cinco
+    //quantos são divisiveis por 3
---
+++ ex02.js
@@ -0,0 +1,15 @@
+prompt = require("prompt-sync")();
+
+console.log("digite as idades das pessoas")
+let i1 = parseInt(prompt("Insira a idade 1 "));
+let i2 = parseInt(prompt("Insira a idade 2 "));
+let i3 = parseInt(prompt("Insira a idade 3 "));
+let i4 = parseInt(prompt("Insira a idade 4 "));
+let i5 = parseInt(prompt("Insira a idade 5 "));
+let i6 = parseInt(prompt("Insira a idade 6 "));
+let i7 = parseInt(prompt("Insira a idade 7 "));
+let i8 = parseInt(prompt("Insira a idade 8 "));
+let i9 = parseInt(prompt("Insira a idade 9 "));
+let i10 = parseInt(prompt("Insira a idade 10 "));
+let media = (i1 + i2 + i3 + i4+ i5 + i6 + i7 + i8 + i9 + i10)
+
